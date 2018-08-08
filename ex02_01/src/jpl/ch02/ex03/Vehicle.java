4e3162382738d051421daff5d89112dd5a9c71a3
tomosugi00 <cs.fjryo@gmail.com>
2018-08-08 23:40:57 +0900

    ex03追加

---
+++ ex02_01/src/jpl/ch02/ex03/Vehicle.java
@@ -0,0 +1,15 @@
+package jpl.ch02.ex03;
+
+public class Vehicle {
+	/**　 現在のスピード */
+	public double speed;
+	/**　 現在の角度*/
+	public double angle;
+	/**　 所有者の名前*/
+	public String ornerName;
+	/**　 ID番号 */
+	public String ID;
+	
+	/**　 次の乗り物の識別番号  */
+	public static String nextID;
+}
