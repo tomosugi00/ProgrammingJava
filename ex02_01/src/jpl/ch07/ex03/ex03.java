f218b8b01f806f1753a8c5172964bcbeb67b3945
tomosugi00 <cs.fjryo@gmail.com>
2018-09-30 23:48:58 +0900

    ex03追加

---
+++ ex02_01/src/jpl/ch07/ex03/ex03.java
@@ -0,0 +1,59 @@
+package jpl.ch07.ex03;
+
+public class ex03
+{
+	private final int depth = 13;
+	private int[][] tri = new int[depth][];
+	
+	public static void main(String[] args)
+	{
+		ex03 e = new ex03();
+		e.CreatePascalTriangle();
+		e.PrintPascalTriangle();
+	}
+	
+	private void CreatePascalTriangle()
+	{
+		
+		for (int d = 1; d <=depth; d++)
+		{
+			/* 各行の要素の算出 */
+			int width = d;
+			int[] row = new int[width];
+			for (int w = 1; w <=width; w++)
+			{
+				if (w == 1)
+				{
+					row[w-1] = 1;
+				}
+				else if (w == d)
+				{
+					row[w-1] = 1;
+				}
+				else
+				{
+					row[w-1] = tri[(d-1)-1][(w-1)-1] + tri[(d-1)-1][w-1];
+				}
+			}
+			tri[d-1] = row; 
+		}
+	}
+		
+	private  void PrintPascalTriangle()
+	{
+		for(int d=0;d<depth;d++)
+		{
+			int width = d;
+			for(int w=0;w<=width;w++)
+			{
+				System.out.printf("%4d", tri[d][w]);
+				if(w!=width)
+				{
+					System.out.printf(" ");
+				}
+			}
+			System.out.printf("\n");
+		}
+	}
+
+}
