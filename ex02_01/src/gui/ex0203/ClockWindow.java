ba172db3ca1579ababda91a7906e17fb6b6d0325
tomosugi00 <cs.fjryo@gmail.com>
2018-10-27 18:50:55 +0900

    ドラッグで移動

--- ex02_01/src/gui/ex0203/ClockWindow.java
+++ ex02_01/src/gui/ex0203/ClockWindow.java
@@ -14,6 +14,10 @@ public class ClockWindow extends JWindow
 		Toolkit.getDefaultToolkit().setDynamicLayout(true);
         this.setBackground(Color.BLACK);
         this.setSize(200, 100);
+        DragWindowListener dargWindowListener = new DragWindowListener();
+        this.addMouseListener(dargWindowListener);
+        this.addMouseMotionListener(dargWindowListener);
+        
         panel = new ClockPanel();
         add(panel);
         this.setVisible(true);
