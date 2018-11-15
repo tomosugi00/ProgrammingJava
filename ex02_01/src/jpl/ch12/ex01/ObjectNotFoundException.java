78ee134abeeaf0d0fc783a6f6dcbc48ae3026e16
tomosugi00 <cs.fjryo@gmail.com>
2018-11-16 00:30:44 +0900

    ex01,02追加

---
+++ ex02_01/src/jpl/ch12/ex01/ObjectNotFoundException.java
@@ -0,0 +1,36 @@
+package jpl.ch12.ex01;
+
+public class ObjectNotFoundException extends Exception
+{
+
+	public ObjectNotFoundException()
+	{
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public ObjectNotFoundException(String message)
+	{
+		super(message);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public ObjectNotFoundException(Throwable cause)
+	{
+		super(cause);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public ObjectNotFoundException(String message, Throwable cause)
+	{
+		super(message, cause);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public ObjectNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace)
+	{
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
